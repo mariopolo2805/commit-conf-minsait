1eb8ae5f75b175c067e342899e9ef7154ec22c26
Alberto Rivera Mérida <>
2019-11-20 20:54:56 +0100

    changes in winner component

--- src/app/winner/winner.component.ts
+++ src/app/winner/winner.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./winner.component.scss']
 })
 export class WinnerComponent implements OnInit {
+  public name: string;
+  public id: number;
+  public encrytpEmail: string;
 
   constructor() { }
 
   ngOnInit() {
-  }
+    this.name = 'Juan García Pérez';
+    this.id = 5;
 
+  }
 }
