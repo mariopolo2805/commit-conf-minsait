3370ca43fc533dc670770bb0e4bd6f995e8437da
Mario Antonio Polo Daza <mapolo@indra.es>
2019-11-21 18:31:21 +0100

    fix project name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('commit-conf-randomize app is running!');
+    expect(page.getTitleText()).toEqual('commit-conf-minsait app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'commit-conf-randomize'`, () => {
+  it(`should have as title 'commit-conf-minsait'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('commit-conf-randomize');
+    expect(app.title).toEqual('commit-conf-minsait');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('commit-conf-randomize app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('commit-conf-minsait app is running!');
   });
 });
